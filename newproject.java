834d821865c409568188645da3beab318a493a3c
kathir080813 <kathiravan0808sk@gmail.com>
2024-12-17 01:40:17 -0800

    newproject

---
+++ newproject.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class newproject{
+    public static void main (String args[])
+    {
+
+        Scanner tamil = new Scanner(System.in);
+        int a = tamil.nextInt();
+        tamil.nextLine();
+        String b = tamil.nextLine();
+        String c = tamil.nextLine();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("my address is"+c);
+
+    }
+}
