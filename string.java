84baeb95ac5b2f6ce5f5d85f209e7d9b785280f2
kathir080813 <kathiravan0808sk@gmail.com>
2024-12-18 20:42:08 -0800

    Added string.java

---
+++ string.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class string{
+    public static void main(String[] args) {
+        Scanner fan = new Scanner(System.in);
+        String num1 = fan.nextLine();
+        String num2 = fan.nextLine();
+        if(num1.equals(num2)){
+            System.out.println("true");
+
+        }
+        else{
+            System.out.println("false");
+        }
+      
+
+
+
+
+    }
+}
